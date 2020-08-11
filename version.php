2ff16a468ee4b64067aedcbca7806f1113854633
lushonline <56685070+lushonline@users.noreply.github.com>
2020-08-11 11:06:03 +0100

    v2020071004
    Fixes #9

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2020071003; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2020071004; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300; // Requires this Moodle version v3.1 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'tool_uploadpage'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [];

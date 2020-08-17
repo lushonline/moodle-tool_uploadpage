eb440145303e12cb449316d04be6abfa38756cbb
lushonline <56685070+lushonline@users.noreply.github.com>
2020-08-17 08:05:40 +0100

    Update version.php
    Set Moodle requires to 3.2

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 
 $plugin->version   = 2020081300; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051700; // Requires this Moodle version v3.5 see https://docs.moodle.org/dev/Releases.
+$plugin->requires  = 2016120500; // Requires this Moodle version v3.2 see https://docs.moodle.org/dev/Releases.
 $plugin->component = 'tool_uploadpage'; // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [];
 $plugin->maturity = MATURITY_STABLE;
